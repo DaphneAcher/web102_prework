9d942391ae0288d22b4a722de2dfaf641bf2e1bd
Daphne Acher <191528512+DaphneAcher@users.noreply.github.com>
2025-01-07 16:40:04 -0500

    challenge 6 changes

--- index.js
+++ index.js
@@ -144,13 +144,19 @@ allBtn.addEventListener("click",showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+let unfundedCount = GAMES_JSON.filter(game => {return game.pledged < game.goal}).length;
 
 
 // create a string that explains the number of unfunded games using the ternary operator
 
+const displayStr = `A total of $633,062 for 4 games. Currently, ${unfundedCount > 0 ? unfundedCount : 'no'} remain unfunded. We need your 
+help to fund these amazing games.`
 
 // create a new DOM element containing the template string and append it to the description container
+let templateStr = document.createElement('p');
+templateStr.innerHTML = displayStr;
 
+descriptionContainer.appendChild(templateStr);
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
  * Skills used: spread operator, destructuring, template literals, sort 
